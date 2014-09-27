74a03d2d50e26a7c34742027009b6b9be29797fc
David Gonzalez <malmstein@gmail.com>
2014-09-27 20:18:37 +0100

    skeleton of BrightnessSeekBar

---
+++ library/src/main/java/com/malmstein/fenster/seekbar/BrigthnessSeekBar.java
@@ -0,0 +1,24 @@
+package com.malmstein.fenster.seekbar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.SeekBar;
+
+public class BrigthnessSeekBar extends SeekBar {
+
+    public BrigthnessSeekBar(Context context) {
+        super(context);
+    }
+
+    public BrigthnessSeekBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BrigthnessSeekBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void init(){
+
+    }
+}
